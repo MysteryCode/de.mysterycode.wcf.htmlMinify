284d2a53c31c1e311157c2b4bec29c2e1040d22c
mutec <webmaster@mysterycode.de>
2016-11-22 03:36:45 +0100

    upload

---
+++ files/lib/system/event/listener/HTMLMinifyListener.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace wcf\system\event\listener;
+
+class HTMLMinifyListener implements IParameterizedEventListener {
+	/**
+	 * @inheritdoc
+	 */
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
+		/** @var $eventObj \wcf\util\HeaderUtil */
+		$eventObj::$output = $this->sanitize_output($eventObj::$output);
+	}
+
+	public function sanitize_output($buffer) {
+		$search = array(
+			'/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+			'/[^\S ]+\</s',  // strip whitespaces before tags, except space
+			'/(\s)+/s',       // shorten multiple whitespace sequences
+			'/\/\/\<\!\[CDATA\[/',
+			'/\/\/\]\]\>/'
+		);
+
+		$replace = array(
+			'>',
+			'<',
+			'\\1',
+			'',
+			''
+		);
+
+		$buffer = preg_replace($search, $replace, $buffer);
+
+		return $buffer;
+	}
+}
